94f64e76900f3adbdf2bd8326d230a1dd1deb6b1
Luke Wyman <c-luke.wyman@charter.com>
2022-09-25 21:04:14 -0600

    Added table names to outputs

--- deploy/outputs.tf
+++ deploy/outputs.tf
@@ -2,10 +2,6 @@ output "api_url" {
     value = aws_api_gateway_stage.api.invoke_url
 }
 
-output "todo_lists_table" {
-    value = aws_dynamodb_table.todo_lists
-}
-
-output "todo_items_table" {
-    value = aws_dynamodb_table.todo_items 
+output "todo_items_table_name" {
+    value = aws_dynamodb_table.todo_items.name
 }
